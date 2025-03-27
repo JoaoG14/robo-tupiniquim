260346579bc6d556ac99f2c35ad52abc02373445
João <joaog.alves14@gmail.com>
2025-03-27 14:12:54 -0300

    Implementar funcionalidade do robo e da area

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -17,14 +17,14 @@ namespace RoboTupiniquim.ConsoleApp
             
             Area area = new Area(areaX, areaY);
             
-            Console.WriteLine("\nRobô");
+            Console.WriteLine("\nRobô 1");
             Console.WriteLine("Posição inicial e orientação (X Y O):");
             string[] posicaoInicial1 = Console.ReadLine().Split(' ');
             int x1 = int.Parse(posicaoInicial1[0]);
             int y1 = int.Parse(posicaoInicial1[1]);
             char orientacao1 = char.Parse(posicaoInicial1[2]);
             
-            Console.WriteLine("Instruções para o Robô:");
+            Console.WriteLine("Instruções para o Robô 1:");
             string instrucoes1 = Console.ReadLine();
             
             Robo robo1 = new Robo(x1, y1, orientacao1, area);
@@ -32,8 +32,24 @@ namespace RoboTupiniquim.ConsoleApp
             
             string posicaoFinal1 = robo1.ObterPosicaoFinal();
             
+            Console.WriteLine("\nRobô 2");
+            Console.WriteLine("Posição inicial e orientação (X Y O):");
+            string[] posicaoInicial2 = Console.ReadLine().Split(' ');
+            int x2 = int.Parse(posicaoInicial2[0]);
+            int y2 = int.Parse(posicaoInicial2[1]);
+            char orientacao2 = char.Parse(posicaoInicial2[2]);
+            
+            Console.WriteLine("Instruções para o Robô 2:");
+            string instrucoes2 = Console.ReadLine();
+            
+            Robo robo2 = new Robo(x2, y2, orientacao2, area);
+            robo2.ExecutarInstrucoes(instrucoes2);
+            
+            string posicaoFinal2 = robo2.ObterPosicaoFinal();
+            
             Console.WriteLine("\nResultados:");
-            Console.WriteLine("Posição final do Robô: " + posicaoFinal1);
+            Console.WriteLine("Posição final do Robô 1: " + posicaoFinal1);
+            Console.WriteLine("Posição final do Robô 2: " + posicaoFinal2);
             
             Console.ReadKey();
         }
@@ -41,36 +57,124 @@ namespace RoboTupiniquim.ConsoleApp
     
     class Area
     {
-        // Implementar a lógica para inicializar a área
+        public int LimiteX { get; private set; }
+        public int LimiteY { get; private set; }
+        
+        public Area(int limiteX, int limiteY)
+        {
+            LimiteX = limiteX;
+            LimiteY = limiteY;
+        }
+        
+        public bool PosicaoValida(int x, int y)
+        {
+            return x >= 0 && x <= LimiteX && y >= 0 && y <= LimiteY;
+        }
     }
     
     class Robo
     {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public char Orientacao { get; private set; }
+        private Area _area;
         
         public Robo(int x, int y, char orientacao, Area area)
         {
-            // Implementar a lógica para inicializar o robô
+            X = x;
+            Y = y;
+            Orientacao = orientacao;
+            _area = area;
         }
         
         public void ExecutarInstrucoes(string instrucoes)
         {
-            // Implementar a lógica para executar as instruções
+            foreach (char instrucao in instrucoes)
+            {
+                switch (instrucao)
+                {
+                    case 'M':
+                        MoverParaFrente();
+                        break;
+                    case 'E':
+                        VirarEsquerda();
+                        break;
+                    case 'D':
+                        VirarDireita();
+                        break;
+                }
+            }
         }
         
         private void MoverParaFrente()
         {
-            // Implementar a lógica para mover para frente
+            int novoX = X;
+            int novoY = Y;
+            
+            switch (Orientacao)
+            {
+                case 'N':
+                    novoY++;
+                    break;
+                case 'S':
+                    novoY--;
+                    break;
+                case 'L':
+                    novoX++;
+                    break;
+                case 'O':
+                    novoX--;
+                    break;
+            }
+            
+            if (_area.PosicaoValida(novoX, novoY))
+            {
+                X = novoX;
+                Y = novoY;
+            }
         }
         
         private void VirarEsquerda()
         {
-            // Implementar a lógica para virar para a esquerda
+            switch (Orientacao)
+            {
+                case 'N':
+                    Orientacao = 'O';
+                    break;
+                case 'O':
+                    Orientacao = 'S';
+                    break;
+                case 'S':
+                    Orientacao = 'L';
+                    break;
+                case 'L':
+                    Orientacao = 'N';
+                    break;
+            }
         }
         
         private void VirarDireita()
         {
-            // Implementar a lógica para virar para a direita
+            switch (Orientacao)
+            {
+                case 'N':
+                    Orientacao = 'L';
+                    break;
+                case 'L':
+                    Orientacao = 'S';
+                    break;
+                case 'S':
+                    Orientacao = 'O';
+                    break;
+                case 'O':
+                    Orientacao = 'N';
+                    break;
+            }
         }
         
+        public string ObterPosicaoFinal()
+        {
+            return $"{X} {Y} {Orientacao}";
+        }
     }
 }
